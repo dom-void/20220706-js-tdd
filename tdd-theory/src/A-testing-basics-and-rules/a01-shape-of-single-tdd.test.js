24582302906bd45e269cee105e1315401eb16731
Jabi <michal.jabi@gmail.com>
2022-07-06 13:58:08 +0200

    feat(a01tdd):  step 1: write all tests (describe behavior) fix(types): add @types/jest for code hints.

---
+++ tdd-theory/src/A-testing-basics-and-rules/a01-shape-of-single-tdd.test.js
@@ -0,0 +1,36 @@
+
+// TDD:
+
+// Funkcja która dodaje n liczb.
+
+
+// #1 Najpierw PISZE TESTY - wszystkie, które przyjdą mi do głowy
+test('should add 1 + 2 + 3 + 4 when 4 arguments passed', () => {
+
+    const result = sumAll(1, 2, 3, 4);
+
+    expect(result).toBe(10);
+})
+
+test('should add 100 + 200 + 300 when 3 arguments passed', () => {
+
+    const result = sumAll(100, 200, 300);
+
+    expect(result).toBe(600);
+})
+
+
+test('should be 0 when no arguments given', () => {
+
+    const result = sumAll();
+
+    expect(result).toBe(0);
+})
+
+
+test('should be 2 when only one argument 2 given', () => {
+
+    const result = sumAll(2);
+
+    expect(result).toBe(2);
+})
