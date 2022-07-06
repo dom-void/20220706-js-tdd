64e7f71334590adde6a44162857ee87d37f839f7
Jabi <michal.jabi@gmail.com>
2022-07-06 10:43:51 +0200

    explain: run any JS with - node filename.js

---
+++ tdd-theory/src/0-warm-up/hello-world.js
@@ -0,0 +1,11 @@
+console.log('Hello World!', 2 + 20, 100 + '!', 4000);
+
+// aby uruchomić ten plik wpisujesz:
+// node hello-world.js
+
+// ale uwaga, będąc na ścieżce:
+// \src\0-warm-up
+
+// Zadanie:
+// Napisz w package.json nowy skrypt wymyśl jego nazwę, uruchom 0-warm-up/hello-world.js
+
