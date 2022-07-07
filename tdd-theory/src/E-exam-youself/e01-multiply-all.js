c5a246ad930eccb77a20ffeb8aeae70a96b33a14
Jabi <michal.jabi@gmail.com>
2022-07-07 10:16:17 +0200

    feat(e01): step #2 - implement solution

--- tdd-theory/src/E-exam-youself/e01-multiply-all.js
+++ tdd-theory/src/E-exam-youself/e01-multiply-all.js
@@ -1,6 +1,14 @@
-function multiplyAll() {
-   
-    
+function multiplyAll(...numbers) {
+   if(numbers.length === 0) {
+     return 1;
+   }
+   let result = 1;
+   numbers.forEach((num) => {
+        if(typeof num === 'number') {
+            result = result * num;
+        }
+   })
+   return result;
 }
 
 // To jest "named export" w Node.js
