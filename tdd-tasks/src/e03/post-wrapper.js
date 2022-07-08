8f6f9d388af4f57b5543dda68e49721e977c92b7
Jabi <michal.jabi@gmail.com>
2022-07-08 11:31:03 +0200

    feat[tdd-tasks](e03): implement unhappy path all errors fallback.

--- tdd-tasks/src/e03/post-wrapper.js
+++ tdd-tasks/src/e03/post-wrapper.js
@@ -1,9 +1,17 @@
 export async function postWrapper(postService, postId) {
 
-    const post = await postService.getOne(postId);
+    try {
+        const post = await postService.getOne(postId);
 
-    return `
-    <h4>${post.title} (${post.userId})</h4>
-    <p>${post.body}</p>
-    `
+        return `
+        <h4>${post.title} (${post.userId})</h4>
+        <p>${post.body}</p>
+        `
+    } catch {
+        //  UWAGA - tutaj upraszczamy rzeczywistość, bo zakładamy że każdy błąd z serwera to 404 !!!
+        return `
+            <h4>Error</h4>
+            <p>Post not exist</p>
+        `
+    }
 }
