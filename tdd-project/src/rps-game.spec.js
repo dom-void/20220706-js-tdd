0ff67c5a4f685dd52c91354cdba94954334534c9
Jabi <michal.jabi@gmail.com>
2022-07-08 14:08:13 +0200

    test[tdd-project](rps-game): test cases for the game

--- tdd-project/src/rps-game.spec.js
+++ tdd-project/src/rps-game.spec.js
@@ -2,29 +2,62 @@
 
 describe('Rock - Paper - Scissors', () => {
 
-    it.skip.each([
+    const GAME_CHOICES = ['rock', 'paper', 'scissors']
+    const GOOD_CHOICE = 'paper';
+    const BAD_CHOICE = 'boom!'
+
+    it.each([
         ['scissors', 'scissors'],
         ['paper', 'paper'],
         ['rock', 'rock'],
-    ])('should be DRAW when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {});
+    ])('should be DRAW when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {
+        const result = gameRound(playerPick, cpuPick);
 
-    it.todo('should throw when 1st choice does not exist');
+        expect(result).toBe('DRAW')
+    });
+
+    it('should throw when 1st choice does not exist', () => {
+
+        expect(() => gameRound(BAD_CHOICE, GOOD_CHOICE)).toThrowError(`Nie ma takiego ruchu jak: ${BAD_CHOICE}`)
+    });
     
-    it.todo('should throw when 2st choice does not exist');
+    it('should throw when 2st choice does not exist', () => {
+
+        expect(() => gameRound(GOOD_CHOICE, BAD_CHOICE)).toThrowError(`Nie ma takiego ruchu jak: ${BAD_CHOICE}`)
+    });
     
-    it.skip.each([
+    it.each([
         ['scissors', 'paper'],
         ['paper', 'rock'],
         ['rock', 'scissors'],
-    ])('should be WIN when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {});
+    ])('should be WIN when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {
+        const result = gameRound(playerPick, cpuPick);
+
+        expect(result).toBe('WIN')
+    });
     
-    it.skip.each([
+    it.each([
         ['paper', 'scissors'],
         ['rock', 'paper'],
         ['scissors', 'rock'],
-    ])('should be LOOSE when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {});
-    
-    it.todo('should show game state to EndUser as: "WYGRANA - wybór komputera: rock" when player picks paper, CPU picks rock [RQ-7] ');
+    ])('should be LOOSE when Player picks %s and CPU picks %s', (playerPick, cpuPick) => {
+        const result = gameRound(playerPick, cpuPick);
 
-    it.todo('CPU choice should be in one of rock paper scissors when randomized')
+        expect(result).toBe('LOOSE')
+    });
+    
+    it('should show game state to EndUser as: "WYGRANA - wybór komputera: rock" when player picks paper, CPU picks rock [RQ-7] ', () => {
+        const cpuChoice = 'rock'
+        const state = gameRound('paper', cpuChoice)
+        
+        const result = gameResult(state, cpuChoice);
+
+        expect(result).toBe('WYGRANA - wybór komputera: rock')
+    });
+
+    it('CPU choice should be in one of rock paper scissors when randomized', () => {
+        const cpuChoice = randomCpuChoice();
+
+        expect(GAME_CHOICES).toContain(cpuChoice)
+    })
 })
